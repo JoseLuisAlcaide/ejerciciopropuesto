dffc8f45ff5a3c7a32bc982b966f91be27301031
JoseLuisAlcaide <josealcaide2000@gmail.com>
2020-12-07 14:08:41 +0100

    escuela

--- recursos/script/myscript.js
+++ recursos/script/myscript.js
@@ -1,18 +1,36 @@
 
 
    
-// let textUser = prompt();
+
+// let textUser =prompt(`texto`).split("");
 // let letraUser = prompt();
 
-let textUser = "aadeaaadeaaaaa";
-let letraUser = ["a","e"];
+let textUser ="aaadeaa".split("");
+let letraUser = "a";
 let nLetra = 0
-// console.log(textUser);
+let nConsecutivos=0;
+
+
+console.log(textUser);
+
+for (i=0 ; i<textUser.length ; i++)
+if (textUser[i]==textUser[i+1]){
+    // let nConsecutivos=0
+    console.log(`consecutivo`)
+    nConsecutivos++
+    console.log(nConsecutivos)
+    }else {
+        console.log(`no consecutivo`)
+    
+    }
+
+
+
 function contar(cadena,letra) {
     let posicion=cadena.indexOf(letra);
     let cuenta=0
     while (posicion != -1) {
-        console.info(posicion,letra)
+        console.log(posicion,letra)
         cuenta++
         posicion=cadena.indexOf(letra, posicion+1)
     }
@@ -20,10 +38,13 @@ function contar(cadena,letra) {
     return cuenta
 }
 
+
 for (i=0;i<letraUser.length;i++)
-   nLetra+=contar(textUser,letraUser[i])
-//console.log("El numero de veces que aparece la letra A es "+contar("A"))
+ nLetra+=contar(textUser,letraUser[i])
+
+console.log("El numero de veces que aparece la letra A es "+contar("A"))
 
 console.log(nLetra)
+
 document.write(`<p style="color:green">${textUser}</p>`)
-document.write(`<p>La suma de todas la vocales Mayúsculas es ${nLetra}</p>`)
+document.write(`<p>Las veces que aparece la letra ${letraUser} es ${nLetra}</p>`)
