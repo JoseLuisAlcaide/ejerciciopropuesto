966e5cc9f4921ee2da2a041974187f7326224d78
JoseLuisAlcaide <josealcaide2000@gmail.com>
2020-12-10 12:58:17 +0100

    Ole Ole

--- recursos/script/myscript.js
+++ recursos/script/myscript.js
@@ -1,23 +1,37 @@
 // let textUser ="aedaadeaaadeaa".split("");
 // let letraUser = "a";
 
-const textUser =prompt(`Itroduzca un texto`).split("");
-const letraUser = prompt(`Introduzca letra consecutiva a buscar`);
+const textUserHtml = document.getElementById("textUserHtml");
+const letraUserHtml = document.getElementById("letraUserHtml");
+const btn_calcular = document.getElementById("userBoton");
+const resultado = document.getElementById("resultado");
 
-let contadorFinal=0;
-let contadorTemp=0;
+btn_calcular.addEventListener("click",inicializar);
 
-for (i=0 ; i<textUser.length ; i++)
-if (textUser[i]===letraUser){
-    contadorTemp++;
-    if(contadorTemp>=contadorFinal){
-        contadorFinal=contadorTemp;
-    }
-}else{
-    contadorTemp=0;
+    function inicializar(){
+        const textUser = (textUserHtml.value).split("");
+        const letraUser = letraUserHtml.value;
+        let contadorFinal=0;
+        let contadorTemp=0;
+        // const textUser = prompt(`Itroduzca un texto`).split("");
+        // const letraUser = prompt(`Introduzca letra consecutiva a buscar`);
+
+        for (i=0 ; i<textUser.length ; i++)
+            if (textUser[i]===letraUser){
+                contadorTemp++;
+                if(contadorTemp>=contadorFinal){
+                contadorFinal=contadorTemp;
+                }
+            }else{
+                contadorTemp=0;
+            }
+            
+    console.log(textUser);
+    console.log(contadorTemp);
+    console.log(contadorFinal);
+    resultado.innerHTML="<h2>El numero de veces máximo consecutivo que se repite "+letraUser+" en el texto "+textUserHtml.value+" es "+contadorFinal+" veces</h2>"
+    //document.write(`El numero de veces máximo consecutivo que se repite "${letraUser}" en el texto "${textUser}" es ${contadorFinal} veces`);
+
 }
 
-console.log(textUser);
-console.log(contadorTemp);
-console.log(contadorFinal);
-document.write(`El numero de veces máximo consecutivo que se repite "${letraUser}" en el texto "${textUser}" es ${contadorFinal} veces`);
+
